e64d1fcb3df1b58c66086d0e5d596cdb34f0684e
Willbeckh <wileyhndirangu04@gmail.com>
2022-06-06 00:24:49 +0300

    create index view class

--- ig_app/views.py
+++ ig_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import View
+from django.http import HttpResponse, Http404
 
 # Create your views here.
+class HomeView(View):
+    def get(self, request):
+      return render(request, 'ig_app/index.html')
+            
