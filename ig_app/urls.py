81c6036645e98232d78ba0958cd7195bf3069093
Willbeckh <wileyhndirangu04@gmail.com>
2022-06-07 02:40:27 +0300

    create logout, newpost url conf path

--- ig_app/urls.py
+++ ig_app/urls.py
@@ -2,12 +2,13 @@
 from django.urls import path
 
 # local imports
-from ig_app.views import HomeView, RegisterView, LoginView, LogoutView
+from ig_app.views import HomeView, RegisterView, LoginView, LogoutView, PostView
 
 app_name = 'igapp'  # application namespace
 urlpatterns = [
     path('', HomeView.as_view(), name='index'),
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout')
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('newpost/', PostView.as_view(), name='newpost'),
 ]
