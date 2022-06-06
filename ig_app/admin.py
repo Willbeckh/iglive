6bf6fdece2de80c94524b84eef3685549a7549d0
Willbeckh <wileyhndirangu04@gmail.com>
2022-06-06 23:26:35 +0300

    register post model to admin site

--- ig_app/admin.py
+++ ig_app/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+# local imports
+from ig_app.models import Post, UserProfile
+ 
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['image_name']}),
+        ('Caption', {'fields': ['image_caption']}),
+        ('Image', {'fields': ['image_file']}),
+    ]
+    
+admin.site.register(Post, PostAdmin)
