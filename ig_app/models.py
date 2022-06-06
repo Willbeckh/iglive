b71babc04d7b465fd2208f20bef61bdab2445bb2
Willbeckh <wileyhndirangu04@gmail.com>
2022-06-06 09:52:57 +0300

    create user profile model

--- ig_app/models.py
+++ ig_app/models.py
@@ -1,3 +1,14 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+
+class UserProfile(models.Model):
+    profile_pic = models.ImageField('profile photo', upload_to='profile_pics/', blank=True)
+    bio = models.TextField(max_length=500, blank=True)
+    
+    def __str__(self):
+        return self.bio
+
