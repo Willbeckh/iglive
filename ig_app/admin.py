f90e80d4d3da486e17822f992aa7c151e909a1b4
Willbeckh <wileyhndirangu04@gmail.com>
2022-06-07 02:36:22 +0300

    add post author field tag to admin site

--- ig_app/admin.py
+++ ig_app/admin.py
@@ -2,13 +2,18 @@ from django.contrib import admin
 
 # local imports
 from ig_app.models import Post, UserProfile
- 
+
 # Register your models here.
+
+
 class PostAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {'fields': ['image_name']}),
+        ('author', {'fields': ['user']}),
         ('Caption', {'fields': ['image_caption']}),
         ('Image', {'fields': ['image_file']}),
+
     ]
-    
+
+
 admin.site.register(Post, PostAdmin)
