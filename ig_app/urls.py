f9c722a7f8cc6d53e20f77a93745c0bac9a81bd0
Willbeckh <wileyhndirangu04@gmail.com>
2022-06-06 18:32:43 +0300

    create logout url pathh

--- ig_app/urls.py
+++ ig_app/urls.py
@@ -2,11 +2,12 @@
 from django.urls import path
 
 # local imports
-from ig_app.views import HomeView, RegisterView, LoginView
+from ig_app.views import HomeView, RegisterView, LoginView, LogoutView
 
 app_name = 'igapp'  # application namespace
 urlpatterns = [
     path('', HomeView.as_view(), name='index'),
     path('register/', RegisterView.as_view(), name='register'),
-    path('login/', LoginView.as_view(), name='login')
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout')
 ]
