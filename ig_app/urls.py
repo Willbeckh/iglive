fbe29ded55f300030fc2f93ad8a7a1e43d0475ad
Willbeckh <wileyhndirangu04@gmail.com>
2022-06-06 09:52:45 +0300

    add user register url conf

--- ig_app/urls.py
+++ ig_app/urls.py
@@ -1,9 +1,11 @@
 """this defines the urlconf of the ig application"""
 from django.urls import path
 
-#local imports
-from ig_app.views import HomeView
+# local imports
+from ig_app.views import HomeView, RegisterView
 
+app_name = 'igapp'  # application namespace
 urlpatterns = [
-    path('', HomeView.as_view(), name='index')
+    path('', HomeView.as_view(), name='index'),
+    path('register/', RegisterView.as_view(), name='register'),
 ]
