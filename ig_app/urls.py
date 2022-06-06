3b1e279deb71da6d77dd5608b7c19edc6c169a2b
Willbeckh <wileyhndirangu04@gmail.com>
2022-06-06 16:32:33 +0300

    ch: create login url path

--- ig_app/urls.py
+++ ig_app/urls.py
@@ -2,10 +2,11 @@
 from django.urls import path
 
 # local imports
-from ig_app.views import HomeView, RegisterView
+from ig_app.views import HomeView, RegisterView, LoginView
 
 app_name = 'igapp'  # application namespace
 urlpatterns = [
     path('', HomeView.as_view(), name='index'),
     path('register/', RegisterView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login')
 ]
