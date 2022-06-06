3892b5277231c262d94bedee46c42778307f5216
Willbeckh <wileyhndirangu04@gmail.com>
2022-06-07 02:40:52 +0300

    create new post view class

--- ig_app/views.py
+++ ig_app/views.py
@@ -6,7 +6,7 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 
 # app imports
-from .forms import CreateUserForm
+from .forms import CreateUserForm, CreatePostForm
 from .models import Post
 
 
@@ -26,12 +26,12 @@ class RegisterView(View):
     '''this class handles the register form page and user creation process'''
     try:
         form = CreateUserForm()
+        context = {
+            'form': form
+        }
 
         def get(self, request):
-            context = {
-                'form': self.form
-            }
-            return render(request, 'ig_app/register.html', context)
+            return render(request, 'ig_app/register.html', self.context)
 
         def post(self, request):
             form = CreateUserForm(request.POST)
@@ -75,3 +75,30 @@ class LogoutView(View):
     def get(self, request):
         logout(request)
         return redirect('igapp:index')
+
+
+# add post view
+class PostView(View):
+    form = CreatePostForm()
+    context = {
+        'title': 'New Post',
+        'form': form
+    }
+
+    def get(self, request):
+        return render(request, 'ig_app/post_form.html', self.context)
+
+    # add post
+    def post(self, request):
+        if not request.user.is_staff or not request.user.is_superuser:
+            raise Http404
+        form = CreatePostForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            # form = CreatePostForm()
+            messages.success(request, 'Post added successfully!')
+            return redirect('igapp:index')
+        context = {
+            'form': form
+        }
+        return render(request, 'ig_app/post_form.html', context)
